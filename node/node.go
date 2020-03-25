9f9bb937ea47d419b60ffd7b8c64f35f340fd73e
renaynay <41963722+renaynay@users.noreply.github.com>
2020-03-25 14:44:59 +0100

    move handler creation to pkg node, remove deprecated function

--- node/node.go
+++ node/node.go
@@ -401,7 +401,7 @@ func (n *Node) startHTTP(endpoint string, apis []rpc.API, modules []string, cors
 // AddWebsocketHandler creates the handler stack necessary to handle both http rpc requests and websocket requests
 func (n *Node) AddWebsocketHandler(handler http.Handler, websocket http.Handler) http.Handler {
 	if websocket != nil {
-		return rpc.NewWebsocketUpgradeHandler(handler, websocket)
+		return NewWebsocketUpgradeHandler(handler, websocket)
 	}
 
 	return handler
