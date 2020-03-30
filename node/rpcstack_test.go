4414046701f49151697f678a529cd07c319d98e8
renaynay <41963722+renaynay@users.noreply.github.com>
2020-03-30 09:26:30 +0200

    incomplete test for cors

--- node/rpcstack_test.go
+++ node/rpcstack_test.go
@@ -11,13 +11,13 @@ import (
 
 // TODO do I need tests for the other handlers? (ex. cors, vhosts, gzip)?
 
-func TestNewHTTPHandlerStack_websocket(t *testing.T) {
+func TestNewHTTPHandlerStack_Cors(t *testing.T) {
+	// todo
 	srv := rpc.NewServer()
 	cors := []string{}
 	vhosts := []string{}
 
 	handler := NewHTTPHandlerStack(srv, cors, vhosts)
-	handler = NewWebsocketUpgradeHandler(handler, srv.WebsocketHandler([]string{}))
 	ts := httptest.NewServer(handler)
 	defer ts.Close()
 
@@ -27,6 +27,33 @@ func TestNewHTTPHandlerStack_websocket(t *testing.T) {
 
 		req, _ := http.NewRequest("GET", ts.URL, nil)
 		//req.Header.Set("Content-type", "application/json")
+		// req.Header.Set() todo
+
+
+
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Error("could not issue a GET request to the test http server") // TODO improve error message?
+		}
+		responses <- resp
+	}(responses)
+
+	response := <- responses
+	assert.Equal(t,"websocket", response.Header.Get("Upgrade"))
+}
+
+func TestNewWebsocketUpgradeHandler_websocket(t *testing.T) {
+	srv := rpc.NewServer()
+
+	handler := NewWebsocketUpgradeHandler(nil, srv.WebsocketHandler([]string{}))
+	ts := httptest.NewServer(handler)
+	defer ts.Close()
+
+	responses := make(chan *http.Response)
+	go func(responses chan *http.Response) {
+		client := &http.Client{}
+
+		req, _ := http.NewRequest("GET", ts.URL, nil)
 		req.Header.Set("Connection", "upgrade")
 		req.Header.Set("Upgrade", "websocket")
 		req.Header.Set("Sec-WebSocket-Version", "13")
