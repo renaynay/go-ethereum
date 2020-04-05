cdb009b171a73b4f39ac73643aa658a592f8d7b5
renaynay <41963722+renaynay@users.noreply.github.com>
2020-04-06 00:07:40 +0200

    one forgotten change

--- log/handler.go
+++ log/handler.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Handler defines where and how log records are written.
-// A Logger prints its log records by writing to a handler.
+// A Logger prints its log records by writing to a Handler.
 // Handlers are composable, providing you great flexibility in combining
 // them to achieve the logging structure that suits your applications.
 type Handler interface {
